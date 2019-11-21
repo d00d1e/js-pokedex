0ba6f897b935f2a98736e17048cc3c2ff5ce6c8b
d00d1e <lee.julianay@gmail.com>
2019-11-21 11:52:30 -0800

    loops/conditionals
    practice with loops and conditionals
    updated css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-var repository = [
-  { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
-  { name: "Charmander", height: 6, types: ["fire"] },
-  { name: "Squirtle", height: 5, types: ["water"] }
+var pokeRepository = [
+  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+  { name: "Charmander", height: 0.6, type: ["fire"] },
+  { name: "Squirtle", height: 0.5, type: ["water"] },
+  { name: "Blastoise", height: 1.6, type: ["water"] }
 ];
+
+for (var i = 0; i < pokeRepository.length; i++) {
+  var name = pokeRepository[i].name;
+  var height = pokeRepository[i].height;
+  var size;
+  if (height > 1) {
+    size = "Wow, that's big!";
+    document.write(name + "- Height: " + height + " <--- " + size + "</br>");
+  } else {
+    document.write(name + "- Height: " + height + "</br>");
+  }
+}

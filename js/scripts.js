3f15a2dcf2fbe43c29509d67e6cdae20ec02d325
d00d1e <lee.julianay@gmail.com>
2019-11-25 00:16:50 -0800

    DOM interaction
    refactored forEach() loop
    added function to create button when adding new pokemon to list
    added event listeners to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -7,39 +7,71 @@ var pokeRepository = (function() {
     { name: 'Blastoise', height: 1.6, type: ['water'] }
   ];
 
-  //function to add pokemon to repository
+  //Function to add new pokemon and corresponding button
+  function addListItem(pokemon) {
+    var $pokemonList = document.querySelector('.pokemon-list');
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    $pokemonList.appendChild(listItem);
+
+    buttonClick(button, pokemon);
+  }
+
+  //Function for event listener on button click
+  function buttonClick (button, pokemon) {
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon);
+    })
+  }
+
+  //
+  function showDetails(pokemon) { 
+    console.log(pokemon);
+  }
+
+  //Function to add a pokemon to repository
   function add(pokemon) {
     //add conditional for format
     if (typeof pokemon === 'object') {
       repository.push(pokemon);
-    } 
-    //add else conditional here
+    }
   }
   
-  //function to 
+  //Function to list all pokemon objects in repository 
   function getAll() {
     return repository;
   }
 
+  //Function objects available outside IIFE
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
 
-// forEach loop
-pokeRepository.getAll().forEach(function(pokemon){
-  var name = pokemon.name;
-  var height = pokemon.height;
-  var size;
-  
-  if (height > 1) {
-    size = "Wow, that's big!";
-    document.write("<p>" + name + "- Height: " + height + " <--- " + size + "</p>");
-  } else {
-    document.write("<p>"+ name + "- Height: " + height + "</p>");
-  }
+pokeRepository.getAll().forEach(function(pokemon) {
+  pokeRepository.addListItem(pokemon);
 });
 
+// forEach loop
+// pokeRepository.getAll().forEach(function(pokemon){
+//   var name = pokemon.name;
+//   var height = pokemon.height;
+//   var size;
+  
+//   if (height > 1) {
+//     size = "Wow, that's big!";
+//     document.write("<p>" + name + "- Height: " + height + " <--- " + size + "</p>");
+//   } else {
+//     document.write("<p>"+ name + "- Height: " + height + "</p>");
+//   }
+// });
 
+
+console.log(pokeRepository.addListItem({name: 'Mew', height: 0.4, type: ['psychic']}))
+console.log(pokeRepository.getAll());
